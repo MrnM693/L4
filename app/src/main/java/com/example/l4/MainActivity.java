b7caf960d350b06b2a7535e9d5bb3e4c6367f6aa
MarianM691 <85128873+MarianM691@users.noreply.github.com>
2022-01-10 17:27:37 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/l4/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.l4;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    String s1[], s2[];
+    int images[] = {R.drawable.caine,R.drawable.pisica,R.drawable.cal,R.drawable.vaca,R.drawable.zebra,R.drawable.leu,R.drawable.tigru,R.drawable.magar,R.drawable.elefant,R.drawable.porc};
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        s1 = getResources().getStringArray(R.array.animale);
+        s2 = getResources().getStringArray(R.array.description);
+
+        MyAdapter myAdapter = new MyAdapter(this, s1, s2, images);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
